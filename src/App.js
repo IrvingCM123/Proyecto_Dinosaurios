05a5e809bee4923c93a9c6550d81f59fe1108383
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2023-04-16 08:14:19 -0600

    Cambios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import imageRickMorty from "./img/morty-rick.png";
+import { useState } from "react";
+import Characters from "./components/Characters";
 
 function App() {
+  const [characters, setCharacters] = useState(null);
+
+  const restapi = async () => {
+    const api = await fetch("https://rickandmortyapi.com/api/character");
+    const characterapi = await api.json();
+    setCharacters(characterapi.results);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          Hola Mundo Uwu
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="Contenedor">
+      <div id="Navegacion">
+        <main class="Navegador">
+          <section class="Logo_Pagina">
+            <div class="Logo_Pagina_img">
+              <img src={imageRickMorty} alt=""></img>
+            </div>
+          </section>
+
+          <section class="Nombre_Pagina">
+          <div class="Nombre_Pagina_Text_Defualt">
+              Aplicación React
+            </div>
+          </section>
+
+          <section class="Opciones_Rutas">
+            <div class="Nombre_Pagina_Text_Defualt">
+              Api sobre Rick And Morty Series!
+            </div>
+          </section>
+
+          <section class="Cuenta">
+            
+          </section>
+        </main>
+      </div>
+
+      <div id="Informacion_Docente">
+        <main class="Contenedor">
+          <article class="Contenedor_Tarjeta">
+            <section>
+              <div class="Card_Docente">
+                <div class="Informacion">
+                  <div class="image">
+                    <div class="circle-1"></div>
+                    <div class="circle-2"></div>
+                    <img
+                      src="Logo_Pagina.jpeg"
+                      alt=""
+                      width="70"
+                      height="70"
+                    ></img>
+                  </div>
+
+                  <div class="actions">
+                    <div class="btn hvr-underline-from-center name">
+                      Irving Rafael Conde Marín{" "}
+                    </div>
+                    <div class="btn hvr-underline-from-center info">
+                      Ingeniería de Software, 602
+                    </div>
+                  </div>
+
+                  <div class="actions">
+                    <div class="btn hvr-underline-from-center info">
+                      Matricula{" "}
+                      <span class="Info_Span"> zS20006735</span>
+                    </div>
+                    <div class="btn hvr-underline-from-center info">
+                      Experiencia:{" "}
+                      <span class="Info_Span">
+                        {" "}
+                        Base de Datos No Relacionales 602
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </article>
+        </main>
+      </div>
+
+      <div id="Pantalla_Informacion">
+        {characters ? (
+          <Characters characters={characters} setCharacters={setCharacters} />
+        ) : (
+          <>
+            <button onClick={restapi} className="btn-search">
+              Buscar Personajes 1
+            </button>
+          </>
+        )}
+      </div>
     </div>
   );
 }
